87b65962bd184dbc4c6b1628057e5141cf5ddd66
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-04-20 17:09:30 +0200

    Printing text and image

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 //Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue(
+    {
+    el: '#app',
+    
+    data: {
+
+        titolo: 'La tua frase motivazionnale preferita',
+
+        immagine: 'https://picsum.photos/200',
+
+        frase: 'Non si è mai troppo vecchi per fissare un nuovo obiettivo o per sognare un nuovo sogno'
+        
+    }
+});
